c96aa03bf34807c9bd7ee861637c646fd70c998b
lochie <disrute@gmail.com>
2026-06-25 09:51:51 +1000

    init RSClient

---
+++ RemoteSock/RemoteSock/RemoteSock.cpp
@@ -0,0 +1,12 @@
+﻿// RemoteSock.cpp : Defines the entry point for the application.
+//
+
+#include "RemoteSock.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
